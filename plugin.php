aa7d3573581cb9af23f818ca61b0bd4a6bb5624e
mastaBo <masta.bo@gmail.com>
2019-01-09 00:44:47 +0300

    changed the attributes names in cf7 orders form to correspond to the ids of positions in DB

--- plugin.php
+++ plugin.php
@@ -252,10 +252,7 @@ document.addEventListener( 'wpcf7mailsent', function( event ) {
 				break;
 			}
 		}
-		resultingDate = orderDate.split("-");
-		var theDate = resultingDate[2]+'.'+resultingDate[1];
     	location = 'http://r2d2.local/wp/%D1%80%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F-%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0-%D0%B4%D0%BB%D1%8F-%D1%84%D0%BE%D1%80%D0%BC/orders/?order-date=' + orderDate;
-    	   
     } 
 }, false );
 </script>
